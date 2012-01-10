3ce55d4370cd5dfeedc845acd5f4ecb3845c8ebe
Miles Gould <miles@assyrian.org.uk>
2012-01-10 15:57:08 +0000

    Make the damn thing pass the sanity checks.

--- lib/Games/Dice/Loaded.pm
+++ lib/Games/Dice/Loaded.pm
@@ -1,21 +1,22 @@
 package Games::Dice::Loaded;
 use Moose;
 use List::Util 'max';
+use Carp;
 
 # ABSTRACT: Perl extension to simulate rolling loaded dice
 
-has 'dartboard' => ( is => 'ro', isa => 'Array' );
-has 'aliases' => ( is => 'ro', isa => 'Array' );
+has 'dartboard' => ( is => 'ro', isa => 'ArrayRef' );
+has 'aliases' => ( is => 'ro', isa => 'ArrayRef' );
 has 'num_sides' => ( is => 'ro', isa => 'Num' );
 
-sub BUILD {
-	my ( $self, $params ) = @_;
+around BUILDARGS => sub {
+	my $orig = shift;
+	my $class = shift;
 	# scale so average weight is 1
-	my @weights = @$params;
+	my @weights = @_;
 	my $n = scalar @weights;
-	my $max = max @weights;
 	my $i = 0;
-	@weights = map { [$i++, ($n * $_) / $max] } @weights; 
+	@weights = map { [$i++, $n * $_] } @weights; 
 	my @small = grep { $_->[1] < 1 } @weights;
 	my @large = grep { $_->[1] >= 1 } @weights;
 	my @dartboard; my @aliases;
@@ -35,17 +36,28 @@ sub BUILD {
 		$dartboard[$unused->[0]] = 1;
 		$aliases[$unused->[0]] = $unused->[0];
 	}
-	$self->dartboard(@dartboard);
-	$self->aliases(@aliases);
-}
+	for my $side (0 .. $n - 1) {
+		my $d = $dartboard[$side];
+		croak("Undefined dartboard for side $side") unless defined $d;
+		croak("Height $d too large for side $side") unless $d <= 1;
+		croak("Height $d too small for side $side") unless $d >= 0;
+	}
+	return $class->$orig(
+		dartboard => \@dartboard,
+		aliases => \@aliases,
+		num_sides => $n,
+	);
+};
 
 sub roll {
 	my ($self) = @_;
 	my $side = int(rand $self->num_sides);
 	my $height = rand 1;
-	my @dartboard = $self->dartboard();
+	my @dartboard = @{$self->dartboard()};
+	croak("Dartboard undefined for side $side")
+		unless defined $dartboard[$side];
 	if ($height > $dartboard[$side]) {
-		my @aliases = $self->aliases;
+		my @aliases = @{$self->aliases};
 		return $aliases[$side];
 	} else {
 		return $side;
--- t/01-sanity.t
+++ t/01-sanity.t
@@ -5,7 +5,7 @@ use Games::Dice::Loaded;
 my $die = Games::Dice::Loaded->new(1/6, 1/6, 1/2, 1/12, 1/12);
 is $die->num_sides, 5, "Die has one side per weight";
 my @rolls;
-for my $i (0 .. 1200) {
+for my $i (0 .. 12000) {
 	my $roll = $die->roll;
 	ok $roll >= 0, "Roll $roll >= 0";
 	ok $roll < 5, "Roll $roll < 5";
